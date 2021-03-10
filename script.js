99d43f662b703add4787f3c9117a4027ae594dd0
Kamran <iskama98@mail.ru>
2021-03-10 14:20:41 +0300

    First_part_is_end

---
+++ script.js
@@ -0,0 +1,25 @@
+//Заданеие 1 
+//==========================================
+
+function howMuch(required) {
+   const defNotes = [100, 50, 20, 10];
+   const result = [];
+
+   if (required > 0) {
+      for(let i=0; i < defNotes.length; i++) {
+         let note = defNotes[i];
+
+         while (required - note >= 0) {
+            required -= note;
+            result.push(note);
+         }
+      }
+   } else {
+      alert('Impossible');
+   }
+   return result;
+}
+
+
+console.log(howMuch( required = +prompt () )); //
+
